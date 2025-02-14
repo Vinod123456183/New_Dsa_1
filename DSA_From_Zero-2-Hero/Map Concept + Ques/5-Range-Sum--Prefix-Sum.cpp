527adba7269343347b073f6932d6904dd1e059be
deeppak_1 <115033744+Vinod123456183@users.noreply.github.com>
2025-02-15 00:42:18 +0530

    Create 5-Range-Sum--Prefix-Sum.cpp

---
+++ DSA_From_Zero-2-Hero/Map Concept + Ques/5-Range-Sum--Prefix-Sum.cpp
@@ -0,0 +1,20 @@
+
+vector<int> prefixSum(const vector<int>& nums) {
+    int n = nums.size();
+    vector<int> prefix(n + 1, 0);
+    for (int i = 1; i <= n; ++i) {
+        prefix[i] = prefix[i - 1] + nums[i];
+    }
+    return prefix;
+}
+
+int optimizedSum(const vector<int>& prefix, int l, int r) {
+    return prefix[r] - prefix[l - 1];
+}
+
+int main() {
+    vector<int> nums = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    vector<int> prefix = prefixSum(nums);
+    int l = 8, r = 9; // Example range [l, r]
+    cout << "Optimized Sum: " << optimizedSum(prefix, l, r) << endl;
+}
