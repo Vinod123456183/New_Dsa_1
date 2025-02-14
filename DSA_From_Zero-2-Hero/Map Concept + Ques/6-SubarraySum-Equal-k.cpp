c92561ac063f7bb7be1c36ba3b05dbd5ce1a0aa6
deeppak_1 <115033744+Vinod123456183@users.noreply.github.com>
2025-02-15 01:36:03 +0530

    Update 6-SubarraySum-Equal-k.cpp

--- DSA_From_Zero-2-Hero/Map Concept + Ques/6-SubarraySum-Equal-k.cpp
+++ DSA_From_Zero-2-Hero/Map Concept + Ques/6-SubarraySum-Equal-k.cpp
@@ -1,6 +1,14 @@
 // to find all subarray i = 0 ,  j = i 
 // if subbray me counting krnu h toh below
 
+
+
+
+ arr = [10, 2, -2, -20, 10], k = -10
+Output: 3
+Explaination: Subarrays: arr[0...3], arr[1...4], arr[3...4] have sum exactly equal to -10
+
+
     for (int i = 0; i < n; ++i) {
         int currentSum = 0;
         for (int j = i; j < n; ++j) {
