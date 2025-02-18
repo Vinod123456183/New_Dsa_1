2769425e1f34c1954b77d4fe0ebe444c69cabf60
deeppak_1 <115033744+Vinod123456183@users.noreply.github.com>
2025-02-19 00:37:57 +0530

    Create 520--Detect-Capital--.cpp

---
+++ DSA_From_Zero-2-Hero/Map Concept + Ques/520--Detect-Capital--.cpp
@@ -0,0 +1,8 @@
+// All letters in this word are capitals, like "USA".
+// All letters in this word are not capitals, like "leetcode".
+// Only the first letter in this word is capital, like "Google".
+
+// Count All UpperCase
+// if Count = 1 and s[0] capital 
+// if count == 0 , all small
+// if Count == s.size // all capital
